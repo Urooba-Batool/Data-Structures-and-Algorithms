ff32a0911807d26812b57df3af90029c964ffb49
Urooba Batool <162785493+Urooba-Batool@users.noreply.github.com>
2025-09-10 20:50:00 +0500

    Add files via upload

---
+++ Recursion-DSA/Recursion-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_1
+{
+    internal class Program
+    {
+        static void Series(int s, int n)
+        {
+            if (s > n)
+            {
+                return;
+            }
+            else
+            {
+                Console.Write(s + " ");
+                Series(s * 2, n);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the starting value: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the nth value: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Series from {0} to {1}: ", s, n);
+            Series(s,n);
+        }
+        
+    }
+}
---
+++ Recursion-DSA/Recursion-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_2
+{
+    internal class Program
+    {
+        static void OddSeries(int s, int n)
+        {
+            if(s > n)
+            {
+                return;
+            }
+            else
+            {
+                if (s % 2 != 0)
+                {
+                    if (s % 2 != 0)
+                    {
+                        Console.Write(s + " " + s*s + " ");
+                    }
+                }
+                OddSeries(s + 1, n);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter starting value of series: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the nth value of series: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Odd number square series till {0}: ", n);
+            OddSeries(s, n);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
