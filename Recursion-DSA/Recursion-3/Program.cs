0b86320951bc6acf061fd5e2a8d9692f9e730900
Urooba Batool <162785493+Urooba-Batool@users.noreply.github.com>
2025-09-17 20:34:44 +0500

    Add files via upload

---
+++ Recursion-DSA/Recursion-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_3
+{
+    internal class Program
+    {
+        static int Sum(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (n % 10) + Sum(n / 10);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number for sum: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int add = Sum(number);
+
+            Console.WriteLine("Sum of digits = " + add);
+        }
+    }
+}
+
