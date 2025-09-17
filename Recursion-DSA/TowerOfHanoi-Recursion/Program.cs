aa857c8b4c7805f50534a0eb325e6bd284ef4a2a
Urooba Batool <162785493+Urooba-Batool@users.noreply.github.com>
2025-09-17 20:33:24 +0500

    Add files via upload

---
+++ Recursion-DSA/TowerOfHanoi-Recursion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi_Recursion
+{
+    internal class Program
+    {
+        static int count(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2 * count(n - 1) + 1;
+            }
+        }
+        static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine("Move disk 1 from rod " + from_rod + " to rod " + to_rod);
+                return;
+            }
+            towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+            Console.WriteLine("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+            towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+        }
+        public static void Main(String[] args)
+        {
+            int n = 4;
+            Console.WriteLine("Tower of Hanoi solving steps:");
+            towerOfHanoi(n, 'A', 'C', 'B');
+            int moves = count(n);
+            Console.WriteLine("Total moves = " + moves);
+        }
+    }
+}
---
+++ Recursion-DSA/TowerOfHanoi-Recursion/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TowerOfHanoi-Recursion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TowerOfHanoi-Recursion")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b96e06ca-bb9c-4587-a3e8-327f185aabb5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
