7de6588a81c7d99da58b0e5cb57acff77da7c975
Urooba Batool <162785493+Urooba-Batool@users.noreply.github.com>
2025-09-20 19:22:51 +0500

    Add files via upload

---
+++ Memory-Representation/Memory_Representation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Representation
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)//enable unsafe mode for compiling the code of pointers
+            // open solution explorer > right click > select properties > open Build tab > select Allow unsafe code 
+        {
+            char x = 'a';
+            int y = 100;
+            float z = 50.4f;
+
+            char* ptrX = &x;
+            Console.WriteLine("Address of char x in RAM/Virtual Memory = {0}", (int)ptrX);
+            Console.WriteLine("Value of x = {0}", *ptrX);
+
+            int* ptrY = &y;
+            Console.WriteLine("Address of int in RAM/Virtual Memory = {0}", (int)ptrY);
+            Console.WriteLine("Value of y = {0}", *ptrY);
+
+            float* ptrZ = &z;
+            Console.WriteLine("Address of float in RAM/Virtual Memory = {0}", (int)ptrZ);
+            Console.WriteLine("Value of z = {0}", *ptrZ);
+        }
+    }
+}
---
+++ Memory-Representation/Memory_Representation_Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Representation_Array
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args) //enable unsafe mode for compiling the code of pointers
+        // open solution explorer > right click > select properties > open Build tab > select Allow unsafe code 
+        {
+            char[] letters = { 'A', 'B', 'C', 'D' };
+
+            unsafe
+            {
+                fixed (char* ptr = letters)
+                {
+                    for (int i = 0; i < letters.Length; i++)
+                    {
+                        Console.WriteLine("Address in RAM/Virtual Memory of letters on index {0} = {1}", i, (int)(ptr + i));
+                        Console.WriteLine("Value of letter on index [{0}] = {1}", i, *(ptr + i));
+                    }
+                }
+            }
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Console.WriteLine();
+            unsafe
+            {
+                fixed (int* ptrN  = numbers)
+                {
+                    for(int i = 0; i < numbers.Length;i++)
+                    {
+                        Console.WriteLine("Address in RAM/Virtual Memory of numbers on index {0} = {1}", i, (int)(ptrN + i));
+                        Console.WriteLine("Value of numbers on index [{0}] = {1}", i, *(ptrN + i));
+                    }
+                }
+            }
+
+        }
+    }
+}
